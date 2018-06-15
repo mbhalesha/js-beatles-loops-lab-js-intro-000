4a54d0c4f5173244ffe60fb2c681e625c776abb7
Manisha Bhalesha <mbhalesha@gmail.com>
2018-06-15 04:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ function theBeatlesPlay(musicians, instruments) {
   return players;
 }
 
+<<<<<<< HEAD
 function johnLennonFacts(facts) {
+=======
+function johnLennonFacts(){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+>>>>>>> 2773a421208ff930bbe947e9e6990490140080e5
   var newFacts = [];
   var i = 0;
   while  (i < facts.length) {
@@ -14,6 +19,7 @@ function johnLennonFacts(facts) {
     i++;
   }
   return newFacts;
+<<<<<<< HEAD
 }
 
 function iLoveTheBeatles(number) {
@@ -23,4 +29,6 @@ function iLoveTheBeatles(number) {
     number++;
   } while (number < 15);
   return group;
+=======
+>>>>>>> 2773a421208ff930bbe947e9e6990490140080e5
 }
